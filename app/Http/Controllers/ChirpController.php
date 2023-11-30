460c5138bdba62e63edba4d4fc541d26c8dc0448
Murilo Melo <murilomelo94@outlook.com>
2023-11-30 19:17:02 -0300

    :bug: fix: incorrect ; added last commit

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -15,7 +15,7 @@ class ChirpController extends Controller
     public function index(): View
     {
         return view('chirps.index', [
-            'chirps' => Chirp::with('user')->latest->get();
+            'chirps' => Chirp::with('user')->latest->get(),
         ]);
     }
 
